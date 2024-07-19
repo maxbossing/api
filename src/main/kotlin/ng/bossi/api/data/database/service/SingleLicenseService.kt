d150183b206c7acb742168a1571178408f1bbb13
Max Bossing <info@maxbossing.de>
2024-07-19 18:20:25 +0200

    CRUD implementation for SingleLicenses.kt

---
+++ src/main/kotlin/ng/bossi/api/data/database/service/SingleLicenseService.kt
@@ -0,0 +1,35 @@
+package ng.bossi.api.data.database.service
+
+import ng.bossi.api.data.database.model.Applications
+import ng.bossi.api.data.database.model.SingleLicense
+import ng.bossi.api.data.database.model.SingleLicenses
+import org.jetbrains.exposed.sql.*
+
+class SingleLicenseService(val database: Database): IDatabaseService<Long, SingleLicense> {
+  override suspend fun create(entity: SingleLicense): Long = dbQuery {
+    SingleLicenses.insert {
+      it[SingleLicenses.application] = entity.application
+      it[SingleLicenses.status] = entity.status
+    }[SingleLicenses.id]
+  }
+
+  override suspend fun read(id: Long): SingleLicense? = dbQuery {
+    (SingleLicenses innerJoin Applications).selectAll()
+      .where { SingleLicenses.id eq id }
+      .map { SingleLicense(
+        application = it[Applications.id],
+        status = it[SingleLicenses.status]
+      )}.singleOrNull()
+  }
+
+  override suspend fun update(id: Long, entity: SingleLicense): Boolean = dbQuery {
+    SingleLicenses.update({ SingleLicenses.id eq id }) {
+      it[SingleLicenses.application] = entity.application
+      it[SingleLicenses.status] = entity.status
+    } > 0
+  }
+
+  override suspend fun delete(id: Long): Boolean = dbQuery {
+    SingleLicenses.deleteWhere { Op.build { SingleLicenses.id eq id } } > 0
+  }
+}
