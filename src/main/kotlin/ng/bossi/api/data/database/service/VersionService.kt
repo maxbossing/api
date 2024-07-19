07b7c95e7cbc59d4f0bab79f20821e46c2c29a1d
Max Bossing <info@maxbossing.de>
2024-07-19 18:45:56 +0200

    CRUD implementation for Versions.kt

---
+++ src/main/kotlin/ng/bossi/api/data/database/service/VersionService.kt
@@ -0,0 +1,43 @@
+package ng.bossi.api.data.database.service
+
+import ng.bossi.api.data.database.model.*
+import org.jetbrains.exposed.sql.*
+
+class VersionService(val database: Database) : IDatabaseService<Long, Version> {
+  override suspend fun create(entity: Version): Long = dbQuery {
+    Versions.insert {
+      it[Versions.version] = entity.version
+      it[Versions.codename] = entity.codename
+      it[Versions.application] = entity.application
+      it[Versions.status] = entity.status
+      it[Versions.resource] = entity.resource
+    }[Versions.id]
+  }
+
+  override suspend fun read(id: Long): Version? = dbQuery {
+    (Versions innerJoin Applications innerJoin Resources)
+      .selectAll()
+      .where { Versions.id eq id }
+      .map { Version(
+        version = it[Versions.version],
+        codename = it[Versions.codename],
+        application = it[Applications.id],
+        status = it[Versions.status],
+        resource = it[Resources.id]
+      )}.singleOrNull()
+  }
+
+  override suspend fun update(id: Long, entity: Version): Boolean = dbQuery {
+    Versions.update({ Versions.id eq id }) {
+      it[Versions.version] = entity.version
+      it[Versions.codename] = entity.codename
+      it[Versions.application] = entity.application
+      it[Versions.status] = entity.status
+      it[Versions.resource] = entity.resource
+    } > 0
+  }
+
+  override suspend fun delete(id: Long): Boolean = dbQuery {
+    Versions.deleteWhere { Op.build { Versions.id eq id } } > 0
+  }
+}
